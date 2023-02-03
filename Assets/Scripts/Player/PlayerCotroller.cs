d19e328e9925fd940cc6b9835bb4a8a02265a012
DmitryItyl <theodridspm@gmail.com>
2023-02-03 21:10:31 +0200

    Changed animation to code-first approach

--- Assets/Scripts/Player/PlayerCotroller.cs
+++ Assets/Scripts/Player/PlayerCotroller.cs
@@ -9,17 +9,29 @@ public class PlayerCotroller : MonoBehaviour
     [SerializeField] GameObject axePrefab;
     [SerializeField] float projectileForce;
 
-    public float fireRate = 0.4f;
+    [SerializeField] float fireRate = 0.41f;
 
-    Vector2 movement;
+    private float xAxis;
+    private float yAxis;
 
     private Animator animator;
+    private string currentState;
+
     private Rigidbody2D rb;
     private bool isFacingRight = true;
+    private bool isAttackPressed;
+    private bool isAttacking;
 
     private Transform axeSpawnPoint;
+    private float attackSlowDownModifier = 0.2f;
 
-    void Awake()
+    // Animation states
+    const string PLAYER_ATTACK = "Attack";
+    const string PLAYER_RUN = "Run";
+    const string PLAYER_IDLE = "Idle";
+    const string PLAYER_DEATH = "Death";
+
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -27,30 +39,56 @@ public class PlayerCotroller : MonoBehaviour
         axeSpawnPoint = transform.Find("AxeSpawnPoint");
     }
 
-    private void Start()
-    {
-        StartCoroutine(ShootingRateCoroutine());
-    }
-
     void Update()
     {
-        Move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
 
-        /* if (Input.GetButtonDown("Fire1"))
-         {
-             StartShooting();
-         }*/
-
-
+        isAttackPressed = Input.GetButton("Fire1");
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = movement;
+        Move(xAxis, yAxis);
+
+        //Debug.Log(isAttacking);
+
+
+        if (isAttackPressed)
+        {
+            if (!isAttacking)
+            {
+                isAttacking = true;
+
+                StartShooting();
+            }
+        }
+        else if (isAttacking)
+        {
+            isAttacking = false;
+        }
+
+        if (!isAttacking)
+        {
+            if ((xAxis != 0) || (yAxis != 0))
+            {
+                ChangeAnimationState(PLAYER_RUN);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+        }
     }
 
-    private void Flip(bool playerLooksRight)
+    #region Sprites and animations
+
+    private void Flip(float direction)
     {
+        bool playerLooksRight = direction > 0;
+        if ((direction == 0) || (isFacingRight == playerLooksRight))
+            return;
+
         isFacingRight = playerLooksRight;
         Vector3 theScale = transform.localScale;
 
@@ -62,68 +100,81 @@ public class PlayerCotroller : MonoBehaviour
         transform.localScale = theScale;
     }
 
+    private void ChangeAnimationState(string newState)
+    { 
+        if (currentState == newState)
+            return;
+
+        Debug.Log("Transitioning to: " + newState);
+        animator.Play(newState);
+
+        currentState = newState;
+    }
+
+    #endregion
+
+    #region Movement
+
     private void Move(float horizontal, float vertical)
     {
-        if (horizontal > 0 && !isFacingRight)
-        {
-            Flip(true);
-        }
-        if (horizontal < 0 && isFacingRight)
-        {
-            Flip(false);
-        };
 
-        movement = new Vector2(horizontal, vertical).normalized * moveSpeed;
+        var movement = new Vector2(horizontal, vertical).normalized * moveSpeed;
 
-        if (movement.magnitude > 0)
-            animator.SetBool("IsMoving", true);
-        else
-            animator.SetBool("IsMoving", false);
+        if (currentState != "Attack")
+            Flip(horizontal);
+
+        rb.velocity = movement;
     }
+
+    #endregion
+
+    #region Attacking
 
     private void StartShooting()
     {
-        animator.SetTrigger("IsAttacking");
+        ChangeAnimationState(PLAYER_ATTACK);
+        moveSpeed *= attackSlowDownModifier;
         StartCoroutine(ThrowAxe());
 
+        Vector3 mousePos = GetMouseWorldPosition(Input.mousePosition) - this.transform.position;
+        Flip(mousePos.x);;
     }
 
     private IEnumerator ThrowAxe()
     {
-        yield return new WaitForSeconds(0.29f);
-
-        Vector3 mousePos = GetMouseWorldPosition(Input.mousePosition) - this.transform.position;
-
-        Vector3 aimDirection = (mousePos).normalized;
-        var aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x);
-        float aimAngleDeg = aimAngle * Mathf.Rad2Deg;
-        axeSpawnPoint.transform.eulerAngles = new Vector3(0, 0, aimAngleDeg);
-
-        GameObject bullet = Instantiate(axePrefab, axeSpawnPoint.position, Quaternion.identity);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
-        float xComponent = Mathf.Cos(aimAngle) * projectileForce;
-        float zComponent = Mathf.Sin(aimAngle) * projectileForce;
-
-        Vector2 forceApplied = new Vector2(xComponent, zComponent);
-        rb.AddForce(forceApplied, ForceMode2D.Impulse);
-    }
-
-    IEnumerator ShootingRateCoroutine()
-    {
-        while (true)  //while (player.isActiveAndEnabled)
+        while (isAttacking)
         {
-            if (Input.GetButton("Fire1"))
-            {
-                StartShooting();
-                yield return new WaitForSeconds(fireRate);
-            }
-            else
-                yield return new WaitForSeconds(0.04f);
+            yield return new WaitForSeconds(0.29f);
+            Vector3 mousePos = GetMouseWorldPosition(Input.mousePosition) - this.transform.position;
+            Flip(mousePos.x);
+
+            Vector3 aimDirection = (mousePos).normalized;
+            var aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x);
+            float aimAngleDeg = aimAngle * Mathf.Rad2Deg;
+            axeSpawnPoint.transform.eulerAngles = new Vector3(0, 0, aimAngleDeg);
+
+            GameObject bullet = Instantiate(axePrefab, axeSpawnPoint.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+            float xComponent = Mathf.Cos(aimAngle) * projectileForce;
+            float zComponent = Mathf.Sin(aimAngle) * projectileForce;
+
+            Vector2 forceApplied = new Vector2(xComponent, zComponent);
+            rb.AddForce(forceApplied, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(0.12f);
         }
 
+        AttackComplete();
     }
 
+    #endregion
+
+    void AttackComplete()
+    {
+        moveSpeed /= attackSlowDownModifier;
+        isAttacking = false;
+    }
 
     // utility method: get mouse world position
     private Vector3 GetMouseWorldPosition(Vector3 screenPosition)
